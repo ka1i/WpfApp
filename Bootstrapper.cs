9f460003eb6a3824fbe8b3e0f93b0dcf225dd85d
ka1i <Mardan137@outlook.com>
2023-07-12 16:54:27 +0800

    feat: use prism support MVVM

---
+++ Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+
+namespace WpfApp
+{
+    internal class Bootstrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
